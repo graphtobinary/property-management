8607ad7e566d419ed00b91ca683236113f715285
Prashant Pandey <prashant.krishna52@gmail.com>
2025-03-24 22:13:48 +0530

    fix build error

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import "./index.css";
 import "swiper/swiper-bundle.css";
 import "simplebar-react/dist/simplebar.min.css";
 import "flatpickr/dist/flatpickr.css";
-import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import App from "./app.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
